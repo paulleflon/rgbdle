c47c1f69f8084fb72694e5d1a3f2b20e9dce4457
hickatheworld <cc@hicka.world>
2022-02-26 21:58:24 +0100

    feat(index): add meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -168,6 +168,30 @@ const Home = ({ colors }: { colors: Record<string, ColorInfo> }) => {
 	return (
 		<div className='relative h-full overflow-hidden'>
 			<Head>
+				{/* Visual assets */}
+				<link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+				<link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+				<link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+				<link rel='manifest' href='/site.webmanifest' />
+				<link rel='mask-icon' href='/safari-pinned-tab.svg' color='#1e293b' />
+				<link rel='shortcut icon' href='/favicon.ico' />
+				<meta name='msapplication-TileColor' content='#1e293b' />
+				<meta name='msapplication-config' content='/browserconfig.xml' />
+				<meta name='theme-color' content='#1e293b' />
+				{/* OpenGraph meta */}
+				<meta property='og:title' content='RGBdle' />
+				<meta property='og:site_name' content='RGBdle' />
+				<meta property='og:description' content='Demonstrate your skills in design: Guess a RGB code every day!' />
+				<meta property='og:type' content='website' />
+				<meta property='og:url' content='https://rgbdle.hicka.world' />
+				<meta property='og:image' content='https://bd7c-81-48-179-225.ngrok.io/card.png' />
+				{/* Twitter meta */}
+				<meta name='twitter:title' content='RGBdle' />
+				<meta name='twitter:description' content='Demonstrate your skills in design: Guess a RGB code every day!' />
+				<meta name='twitter:image' content='https://rgbdle.hicka.world/card.png' />
+				<meta name='twitter:image:alt' content='RGBdle - Demonstrate your skills in design: Guess a RGB code every day!' />
+				<meta name='twitter:card' content='summary_large_image' />
+				<meta name='twitter:site' content='@hickatheworld' />
 				<title>RGBdle</title>
 			</Head>
 			{
