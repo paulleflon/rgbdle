fe522676aa0dfe128ad11d18038a1bff4207b9ac
hickatheworld <cc@hicka.world>
2022-02-26 22:04:15 +0100

    feat(index): keep meta tags in maintenance mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -142,57 +142,64 @@ const Home = ({ colors }: { colors: Record<string, ColorInfo> }) => {
 		localStorage.setItem('RGBDLE_LAST_IGNORED_WARNING', process.env.NEXT_PUBLIC_WARNING!);
 	};
 
+	const head = (
+		<Head>
+			{/* Visual assets */}
+			<link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+			<link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+			<link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+			<link rel='manifest' href='/site.webmanifest' />
+			<link rel='mask-icon' href='/safari-pinned-tab.svg' color='#1e293b' />
+			<link rel='shortcut icon' href='/favicon.ico' />
+			<meta name='msapplication-TileColor' content='#1e293b' />
+			<meta name='msapplication-config' content='/browserconfig.xml' />
+			<meta name='theme-color' content='#1e293b' />
+			{/* OpenGraph meta */}
+			<meta property='og:title' content='RGBdle' />
+			<meta property='og:site_name' content='RGBdle' />
+			<meta property='og:description' content='Demonstrate your skills in design: Guess a RGB code every day!' />
+			<meta property='og:type' content='website' />
+			<meta property='og:url' content='https://rgbdle.hicka.world' />
+			<meta property='og:image' content='https://rgbdle.hicka.world/card.png' />
+			{/* Twitter meta */}
+			<meta name='twitter:title' content='RGBdle' />
+			<meta name='twitter:description' content='Demonstrate your skills in design: Guess a RGB code every day!' />
+			<meta name='twitter:image' content='https://rgbdle.hicka.world/card.png' />
+			<meta name='twitter:image:alt' content='RGBdle - Demonstrate your skills in design: Guess a RGB code every day!' />
+			<meta name='twitter:card' content='summary_large_image' />
+			<meta name='twitter:site' content='@hickatheworld' />
+			<title>RGBdle</title>
+		</Head>
+	);
+
 	if (process.env.NEXT_PUBLIC_MAINTENANCE === '1') {
 		return (
-			<div className='flex flex-col justify-center items-center h-full'>
-				<div className='font-title text-lg md:text-4xl my-7'>RGBdle is currently under maintenance.</div>
-				<div className='font-body text-lg md:text-3xl'>Please try again later!</div>
-				{
-					process.env.NEXT_PUBLIC_MAINTENANCE_MESSAGE &&
-					(
-						<>
-							<div className='my-10 w-1/2 min-w-1/3 h-px bg-slate-500'></div>
-							<div
-								className='font-[monospace] text-sm md:text-xl text-center px-20'
-								dangerouslySetInnerHTML={{ __html: process.env.NEXT_PUBLIC_MAINTENANCE_MESSAGE }}
-							>
-							</div>
-						</>
-					)
-				}
-			</div>
+			<>
+				{head}
+				<div className='flex flex-col justify-center items-center h-full'>
+					<div className='font-title text-lg md:text-4xl my-7'>RGBdle is currently under maintenance.</div>
+					<div className='font-body text-lg md:text-3xl'>Please try again later!</div>
+					{
+						process.env.NEXT_PUBLIC_MAINTENANCE_MESSAGE &&
+						(
+							<>
+								<div className='my-10 w-1/2 min-w-1/3 h-px bg-slate-500'></div>
+								<div
+									className='font-[monospace] text-sm md:text-xl text-center px-20'
+									dangerouslySetInnerHTML={{ __html: process.env.NEXT_PUBLIC_MAINTENANCE_MESSAGE }}
+								>
+								</div>
+							</>
+						)
+					}
+				</div>
+			</>
 		)
 	}
 
 	return (
 		<div className='relative h-full overflow-hidden'>
-			<Head>
-				{/* Visual assets */}
-				<link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
-				<link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
-				<link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
-				<link rel='manifest' href='/site.webmanifest' />
-				<link rel='mask-icon' href='/safari-pinned-tab.svg' color='#1e293b' />
-				<link rel='shortcut icon' href='/favicon.ico' />
-				<meta name='msapplication-TileColor' content='#1e293b' />
-				<meta name='msapplication-config' content='/browserconfig.xml' />
-				<meta name='theme-color' content='#1e293b' />
-				{/* OpenGraph meta */}
-				<meta property='og:title' content='RGBdle' />
-				<meta property='og:site_name' content='RGBdle' />
-				<meta property='og:description' content='Demonstrate your skills in design: Guess a RGB code every day!' />
-				<meta property='og:type' content='website' />
-				<meta property='og:url' content='https://rgbdle.hicka.world' />
-				<meta property='og:image' content='https://rgbdle.hicka.world/card.png' />
-				{/* Twitter meta */}
-				<meta name='twitter:title' content='RGBdle' />
-				<meta name='twitter:description' content='Demonstrate your skills in design: Guess a RGB code every day!' />
-				<meta name='twitter:image' content='https://rgbdle.hicka.world/card.png' />
-				<meta name='twitter:image:alt' content='RGBdle - Demonstrate your skills in design: Guess a RGB code every day!' />
-				<meta name='twitter:card' content='summary_large_image' />
-				<meta name='twitter:site' content='@hickatheworld' />
-				<title>RGBdle</title>
-			</Head>
+			{head}
 			{
 				isLoading ?
 					<div className='flex justify-center my-4'>
