95a47851b8f7611bbfab9743255d226752d8cb16
hickatheworld <cc@hicka.world>
2022-03-23 21:10:27 +0100

    fix(GuessCell): fix wrong indicators

--- components/GuessCell.tsx
+++ components/GuessCell.tsx
@@ -8,15 +8,15 @@ import GuessCellProps from '../interfaces/GuessCellProps';
 function GuessCell({ expected, guess, inactive }: GuessCellProps) {
 	let backgroundColor;
 	let valueIndicator;
-	if (!inactive && guess) {
-		backgroundColor = (expected === guess) ? '#49da1e' : (expected < guess) ? '#da1e8d' : '#e9900a';
+	if (!inactive) {
+		backgroundColor = (expected === guess) ? '#49da1e' : (expected < guess!) ? '#e9900a' : '#da1e8d';
 		// The MdDone icon looks too big in size 24, but we have to give all icons the same size for alignment.
-		valueIndicator = (expected === guess) ? <MdDone size={24} className='scale-[0.9]' /> : (expected < guess) ? <HiOutlinePlusSm size={24} /> : <HiMinusSm size={24} />;
+		valueIndicator = (expected === guess) ? <MdDone size={24} className='scale-[0.9]' /> : (expected < guess!) ? <HiMinusSm size={24} /> : <HiOutlinePlusSm size={24} />;
 	}
 	return (
 		<div
 			className='flex flex-col items-center justify-center mx-2 rounded w-12 h-12 font-title text-center text-white'
-			style={{ backgroundColor: backgroundColor || '#D1D5DB'}}
+			style={{ backgroundColor: backgroundColor || '#D1D5DB' }}
 		>
 			{inactive ||
 				<>
