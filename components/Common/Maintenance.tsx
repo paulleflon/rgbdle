6ac6ecfba9c0b8341d969db917ed8eff4594ced0
hickatheworld <cc@hicka.world>
2022-03-26 15:03:34 +0100

    feat(Common): add links to Maintenance

--- components/Common/Maintenance.tsx
+++ components/Common/Maintenance.tsx
@@ -1,10 +1,18 @@
+import Link from 'next/link';
+
 /**
  * Whole content of the app if it is under maintenance.
  */
 const Maintenance = ({ mode }: { mode: 'mania' | 'standard' | 'versus' }) => {
+	const modes = {
+		// [Path, Page title]
+		'standard': ['/', 'RGBdle'],
+		'mania': ['/mania', 'RGBdle Mania']
+		// Versus will be added when it's actually implemented.
+	}
 	return (
 		<div className='flex flex-col justify-center items-center h-full'>
-			<div className='font-title text-lg md:text-4xl my-7'>RGBdle {mode !== 'standard' && mode.toUpperCase()} is currently under maintenance.</div>
+			<div className='font-title text-lg md:text-4xl my-7'>{modes[mode as keyof typeof modes][1]} is currently under maintenance.</div>
 			<div className='font-body text-lg md:text-3xl'>Please try again later!</div>
 			{
 				process.env.NEXT_PUBLIC_MAINTENANCE_MESSAGE &&
@@ -15,6 +23,17 @@ const Maintenance = ({ mode }: { mode: 'mania' | 'standard' | 'versus' }) => {
 							className='font-[monospace] text-sm md:text-xl text-center px-20'
 							dangerouslySetInnerHTML={{ __html: process.env.NEXT_PUBLIC_MAINTENANCE_MESSAGE }}
 						>
+						</div>
+						<div className='absolute bottom-2 text-center text-white/50 text-sm'>
+							{
+								Object.entries(modes).map(([k, v]) => (
+									k !== mode && (
+										<Link href={v[0]} key={k}>
+											<a>{v[1]}</a>
+										</Link>
+									)
+								))
+							}
 						</div>
 					</>
 				)
