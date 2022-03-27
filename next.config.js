18094f89a18099b29a773d6bf77b0f679d98e9cc
Hicka <cc@hicka.world>
2022-03-27 22:37:33 +0200

    chore(next): add permanent redirection to /mania

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	async redirectos() {
+		return [
+			{
+				source: '/(.*)',
+				destination: '/mania',
+				permanent: true
+			}
+		];
+	}
 };
 
 module.exports = nextConfig;
