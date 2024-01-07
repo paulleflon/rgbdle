8c2a414ea9372b50ef15ca6ee3853db093f2590d
hickatheworld <cc@hicka.world>
2024-01-07 18:14:22 +0100

    feat(Guide): update Guide explainations and example

--- components/Common/Guide.tsx
+++ components/Common/Guide.tsx
@@ -15,11 +15,16 @@ const Guide = ({ close, displayed }: PopupProps): ReactElement => {
 				</div>
 				<div className='my-4 text-xs md:text-lg leading-5'>
 					<div>
-						In computers, colors are represented by a combination of three values: red, green, and blue.
-						Each of them is an integer between 0 and 255.
+						In computers, colors are usually represented as a combination of three values:
 					</div>
-					<div className='font-bold'>
-						Your mission is to guess the three values required to generate the given color.
+					<div>
+						<b className='text-[#ff2e2e]'> red</b>,
+						<b className='text-[#3bdb40]'> green</b>, and
+						<b className='text-[#3770db]'> blue</b>.
+					</div>
+						Each value is an integer between <b>0</b> and <b>255</b>.
+					<div className='font-bold bold'>
+						Your goal is to guess the Red, Green, and Blue values of the given golor!
 					</div>
 					<div className='w-[90%] mx-[5%] my-4 h-px bg-slate-500'></div>
 					<div className='font-title text-sm md:text-2xl'>
@@ -27,8 +32,7 @@ const Guide = ({ close, displayed }: PopupProps): ReactElement => {
 					</div>
 					<div className='text-sm'>
 						<div
-							className='mt-2 flex flex-row justify-center items-center h-6'
-						>
+							className='mt-2 flex flex-row justify-center items-center h-6'>
 							{
 								['R', 'G', 'B'].map(c =>
 									<div className='mx-2 w-12 text-center font-title text-xl' key={c}>
@@ -47,18 +51,22 @@ const Guide = ({ close, displayed }: PopupProps): ReactElement => {
 							status='past'
 							submitGuess={(): void => {}}
 						/>
+						<ColorRow
+							correct={[25, 125, 225]}
+							guesses={[[25, 125, 225]]}
+							index={0}
+							status='past'
+							submitGuess={(): void => {}}
+						/>
 					</div>
 					<div>
-						Here, the correct values are 25, 125 and 225, and they make <span className='font-bold text-[#1996C8]'>Pacific Blue</span>.
+						A <span className='font-bold text-[#49da1e]'>GREEN</span> cell means the value is <span className='font-bold'>CORRECT</span>.
 					</div>
 					<div>
-						So, a <span className='font-bold text-[#49da1e]'>GREEN</span> cell means <span className='font-bold'>CORRECT</span>.
+						An <span className='font-bold text-[#e9900a]'>ORANGE</span> cell means the correct value is <span className='font-bold'>LOWER</span>.
 					</div>
 					<div>
-						An <span className='font-bold text-[#e9900a]'>ORANGE</span> cell means <span className='font-bold'>LOWER</span>.
-					</div>
-					<div>
-						A <span className='font-bold text-[#da1e8d]'>MAGENTA</span> cell means <span className='font-bold'>HIGHER</span>.
+						A <span className='font-bold text-[#da1e8d]'>MAGENTA</span> cell means the correct value is <span className='font-bold'>HIGHER</span>.
 					</div>
 				</div>
 			</div>
